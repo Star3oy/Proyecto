3822814fb9c3c03ca6515abf260a09709a0cb885
Star3oy <eduarcaco@hotmail.com>
2023-05-09 18:32:47 -0600

    La clase UserDAO arreglada

--- SSPGER/src/mx/uv/fei/implementations/UserDAO.java
+++ SSPGER/src/mx/uv/fei/implementations/UserDAO.java
@@ -25,8 +25,8 @@ public class UserDAO implements IUser{
     private final String MODIFY_USER_COMMAND = "UPDATE usuarios SET idUsuario = ?, nombre = ?, primerApellido = ?, segundoApellido = ?,"
                 + "correoInstitucional = ?, estado = ?, idRol = ? WHERE idUsuario = ?";
     private final String DISABLE_USER_COMMAND = "UPDATE usuarios SET estado = 0 WHERE idUsuario = ?";
-    private final String GET_ACTIVES_USERS_QUERY = "SELECT * FROM usuarios WHERE estado = 1";
-    private final String GET_INACTIVES_USERS_QUERY = "SELECT * FROM usuarios WHERE estado = 0";
+    private final String GET_USERS_BY_STATUS_QUERY = "SELECT * FROM usuarios WHERE estado = ?";
+    private final String VERIFY_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM usuarios WHERE correoInstitucional = ?";
     
     
     @Override
@@ -42,7 +42,7 @@ public class UserDAO implements IUser{
         statement.setString(4,user.getLastName());
         statement.setString(5,user.getInstitutionalEmail());
         statement.setInt(6, user.getIdStatus());
-        statement.setInt(7, user.getIdRole());
+        statement.setInt(7, user.getType());
         result = statement.executeUpdate();
         if (result == 0) {
             throw new SQLException ("Error al añadir usuario");
@@ -55,10 +55,8 @@ public class UserDAO implements IUser{
     @Override
     public List<User> getUserList() throws SQLException {
         List<User> userList = new ArrayList<>();
-        
         String query = GET_ALL_USERS_QUERY;
-        
-       Connection connection = DataBaseManager.getConnection(); 
+        Connection connection = DataBaseManager.getConnection(); 
         Statement statement = connection.createStatement();
         ResultSet result = statement.executeQuery(query);              
             while(result.next()) {
@@ -69,11 +67,10 @@ public class UserDAO implements IUser{
                 user.setLastName(result.getString("segundoApellido"));
                 user.setInstitutionalEmail(result.getString("correoInstitucional"));
                 user.setIdStatus(result.getInt("estado"));
-                user.setIdRole(result.getInt("idRol"));
+                user.setType(result.getInt("tipoUsuario"));
                 userList.add(user);
             }
         DataBaseManager.closeConnection();
-        
         return userList;    
     }
 
@@ -93,7 +90,7 @@ public class UserDAO implements IUser{
         user.setLastName(result.getString("segundoApellido"));
         user.setInstitutionalEmail(result.getString("correoInstitucional"));
         user.setIdStatus(result.getInt("estado"));
-        user.setIdRole(result.getInt("idRol"));
+        user.setType(result.getInt("tipoUsuario"));
         
         DataBaseManager.closeConnection();
         return user;
@@ -112,7 +109,7 @@ public class UserDAO implements IUser{
         statement.setString(4, user.getLastName());
         statement.setString(5, user.getInstitutionalEmail());
         statement.setInt(6, user.getIdStatus());
-        statement.setInt(7, user.getIdRole());
+        statement.setInt(7, user.getType());
         statement.setString(8, idUser);
         result = statement.executeUpdate();
          if (result == 0) {
@@ -145,7 +142,7 @@ public class UserDAO implements IUser{
       int cont = 0;
       String query = "SELECT COUNT(*) FROM usuarios WHERE (idusuario = ? AND nombre = ?) AND"
               +"(primerApellido = ? AND segundoApellido = ?)";
-      Connection connection = DataBaseManager.getConnection();
+        Connection connection = DataBaseManager.getConnection();
           
         PreparedStatement statement = connection.prepareStatement(query);
         
@@ -165,7 +162,7 @@ public class UserDAO implements IUser{
     @Override
     public int verifyUserEmail(String email) throws SQLException {
       int cont = 0;
-      String query = "SELECT COUNT(*) FROM usuarios WHERE correoInstitucional = ?";
+      String query = VERIFY_USER_EMAIL_QUERY;
       Connection connection = DataBaseManager.getConnection();
         PreparedStatement statement = connection.prepareStatement(query);
         statement.setString(1, email);
@@ -177,53 +174,21 @@ public class UserDAO implements IUser{
     }
 
     @Override
-    public List<User> getActiveUsers() throws SQLException {
-         List<User> userList = new ArrayList<>();
-        
-        String query = GET_ACTIVES_USERS_QUERY;
-        
-      Connection connection = DataBaseManager.getConnection(); 
-        Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery(query);
-                    
+    public List<User> getUsersByStatus(int status) throws SQLException {  
+        List<User> userList = new ArrayList<>(); 
+        Connection connection = DataBaseManager.getConnection();
+        String query = GET_USERS_BY_STATUS_QUERY;
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, status);
+        ResultSet result = statement.executeQuery();    
+        result.next();
             while(result.next()) {
                 User user = new User();
                 user.setIdUser(result.getString("idUsuario"));
                 user.setFirstName(result.getString("nombre"));
-                user.setMiddleName(result.getString("primerApellido"));
-                user.setLastName(result.getString("segundoApellido"));
-                user.setInstitutionalEmail(result.getString("correoInstitucional"));
-                user.setIdStatus(result.getInt("estado"));
-                user.setIdRole(result.getInt("idRol"));
                 userList.add(user);
             }        
          DataBaseManager.closeConnection();
-        return userList;
+         return userList;
     }
-
-    @Override
-    public List<User> getInactiveUsers() throws SQLException {
- List<User> userList = new ArrayList<>();
-        
-        String query = GET_INACTIVES_USERS_QUERY;
-        
-        Connection connection = DataBaseManager.getConnection(); 
-        Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery(query);
-                    
-            while(result.next()) {
-                User user = new User();
-                user.setIdUser(result.getString("idUsuario"));
-                user.setFirstName(result.getString("nombre"));
-                user.setMiddleName(result.getString("primerApellido"));
-                user.setLastName(result.getString("segundoApellido"));
-                user.setInstitutionalEmail(result.getString("correoInstitucional"));
-                user.setIdStatus(result.getInt("estado"));
-                user.setIdRole(result.getInt("idRol"));
-                userList.add(user);
-            }
-         DataBaseManager.closeConnection();
-        return userList;    
-    }
-  
 }
